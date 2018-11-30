5a264697939321d4d88957b6bf5b257991ea66f2
Valo Erkka A <evalo@lx6-clin3710-01.kll.helsinki.fi>
2018-11-30 14:47:39 +0200

    Finished data wrangling for exercise 5.

--- data/create_human.R
+++ data/create_human.R
@@ -1,6 +1,6 @@
 # Erkka Valo
-# 2018.11.07
-# Create a dataset in exercise 4. of IODS course.
+# 2018.11.30
+# Create a dataset in exercise 4 and 5 of IODS course.
 # Dataset meta information:
 # http://hdr.undp.org/en/content/human-development-index-hdi
 # http://hdr.undp.org/sites/default/files/hdr2015_technical_notes.pdf
@@ -9,6 +9,7 @@
 
 # Load needed libraries
 library(dplyr)
+library(tidyr)
 
 # Read in the data.
 ####################
@@ -79,7 +80,7 @@ gii <- rename(gii,
               Labo.M=Labour.Force.Participation.Rate..Male.)
 
 
-# Define a new column alc_use by combining weekday and weekend alcohol use
+# Define new columns
 gii <- mutate(gii, Edu2.FM = Edu2.F / Edu2.M)
 gii <- mutate(gii, Labo.FM = Labo.F / Labo.M)
 
@@ -89,6 +90,82 @@ human <- inner_join(hd, gii, by = 'Country')
 # Check the dimensions, the dataset should have 195 observations and 19 variables
 dim(human)
 # [1] 195 19
+
+# Exercise 5: continue data wrangling
+#######################################
+
+# Structure of the data
+str(human)
+# 'data.frame':	195 obs. of  19 variables:
+#  $ HDI.Rank      : int  1 2 3 4 5 6 6 8 9 9 ...
+#  $ Country       : chr  "Norway" "Australia" "Switzerland" "Denmark" ...
+#  $ HDI           : num  0.944 0.935 0.93 0.923 0.922 0.916 0.916 0.915 0.913 0.913 ...
+#  $ Life.Exp      : num  81.6 82.4 83 80.2 81.6 80.9 80.9 79.1 82 81.8 ...
+#  $ Edu.Exp       : num  17.5 20.2 15.8 18.7 17.9 16.5 18.6 16.5 15.9 19.2 ...
+#  $ Edu.Mean      : num  12.6 13 12.8 12.7 11.9 13.1 12.2 12.9 13 12.5 ...
+#  $ GNI           : chr  "64,992" "42,261" "56,431" "44,025" ...
+#  $ GNI.m.HDI.rank: int  5 17 6 11 9 11 16 3 11 23 ...
+#  $ GII.Rank      : int  1 2 3 4 5 6 6 8 9 9 ...
+#  $ GII           : num  0.067 0.11 0.028 0.048 0.062 0.041 0.113 0.28 0.129 0.157 ...
+#  $ Mat.Mor       : int  4 6 6 5 6 7 9 28 11 8 ...
+#  $ Ado.Birth     : num  7.8 12.1 1.9 5.1 6.2 3.8 8.2 31 14.5 25.3 ...
+#  $ Parli.F       : num  39.6 30.5 28.5 38 36.9 36.9 19.9 19.4 28.2 31.4 ...
+#  $ Edu2.F        : num  97.4 94.3 95 95.5 87.7 96.3 80.5 95.1 100 95 ...
+#  $ Edu2.M        : num  96.7 94.6 96.6 96.6 90.5 97 78.6 94.8 100 95.3 ...
+#  $ Labo.F        : num  61.2 58.8 61.8 58.7 58.5 53.6 53.1 56.3 61.6 62 ...
+#  $ Labo.M        : num  68.7 71.8 74.9 66.4 70.6 66.4 68.1 68.9 71 73.8 ...
+#  $ Edu2.FM       : num  1.007 0.997 0.983 0.989 0.969 ...
+#  $ Labo.FM       : num  0.891 0.819 0.825 0.884 0.829 ...
+
+# The data combines several indicators from most countries in the world
+#
+# "Country" = Country name
+# 
+# # Health and knowledge
+# 
+# "GNI" = Gross National Income per capita
+# "Life.Exp" = Life expectancy at birth
+# "Edu.Exp" = Expected years of schooling 
+# "Mat.Mor" = Maternal mortality ratio
+# "Ado.Birth" = Adolescent birth rate
+# 
+# # Empowerment
+# 
+# "Parli.F" = Percetange of female representatives in parliament
+# "Edu2.F" = Proportion of females with at least secondary education
+# "Edu2.M" = Proportion of males with at least secondary education
+# "Labo.F" = Proportion of females in the labour force
+# "Labo.M" " Proportion of males in the labour force
+#
+# "Edu2.FM" = Edu2.F / Edu2.M
+# "Labo.FM" = Labo2.F / Labo2.M
+
+# Remove the commas used as thousands-separator and transform 
+# Gross National Income (GNI) to numeric
+human <- mutate(human, GNI= as.numeric(sub(",", "", GNI)))
+
+
+# Remove unneeded variables.
+keep_vars <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", 
+                "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- dplyr::select(human, one_of(keep_vars))
+
+# Remove all rows with missing values
+human <- drop_na(human)
+
+# Remove the observations which relate to regions instead of countries
+regions <- c("Arab States", "East Asia and the Pacific", 
+             "Europe and Central Asia", "Latin America and the Caribbean", 
+             "South Asia", "Sub-Saharan Africa", "World")
+human <- filter(human, !Country %in% regions)
+
+# Set the country names as row names and remove 'Country' variable
+rownames(human) <- human$Country
+human <- select(human, -Country)
+
+# Check that dimensions of the dataset are right
+dim(human)
+# [1] 155   8
 
 # Set the working directory to IODS project folder
 setwd("~/git/IODS-project/")
